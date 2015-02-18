2e026f42212adac3dee5c7c0bddb4e75d445101a
phitchcock <phitchcock@me.com>
2015-02-17 18:51:14 -0800

    updated controller actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,45 @@
 class ArticlesController < ApplicationController
 
-  before_action :article_params, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
+    @articles = Article.all
   end
 
   def show
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @article.destroy
+      redirect_to root_path
+    else
+      redirect_to @article
+    end
   end
 
   private
@@ -30,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit!
+    params.require(:article).permit(:title, :body)
   end
 
 end
