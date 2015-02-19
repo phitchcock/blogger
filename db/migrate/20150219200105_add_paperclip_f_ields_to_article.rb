627d138229c1b49637b3bd607d079c96a4ac0af6
phitchcock <phitchcock@me.com>
2015-02-19 12:07:53 -0800

    installed paperclip

---
+++ db/migrate/20150219200105_add_paperclip_f_ields_to_article.rb
@@ -0,0 +1,8 @@
+class AddPaperclipFIeldsToArticle < ActiveRecord::Migration
+  def change
+    add_column :articles, :image_file_name, :string
+    add_column :articles, :image_content_type, :string
+    add_column :articles, :image_file_size, :integer
+    add_column :articles, :image_updated_at, :string
+  end
+end
